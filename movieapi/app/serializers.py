1da9659c9903b87421039ada0eb01fe51ff5e186
Jaakko Seppänen <seppanenj91@gmail.com>
2019-02-06 19:19:32 +0200

    Added rest of the models and serialized them

--- movieapi/app/serializers.py
+++ movieapi/app/serializers.py
@@ -1,20 +1,23 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from movieapi.app.models import Movie, Review
+from movieapi.app.models import Movie, Review, Comment, Actor, MovieActor, Trailer, Category, MovieCategory
 
 """
 TODO:
 - Comment
-- Add all the models
+- Program actual rating for movie
+- Make a proper timestamp format for comments (00:00:00)
 """
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     #Because 'reviews' is a reverse relationship on the User model, it will not be included by default when using the ModelSerializer class, so we needed to add an explicit field for it.
     reviews = serializers.HyperlinkedRelatedField(many=True, view_name='review-detail', read_only=True)
+    comments = serializers.HyperlinkedRelatedField(many=True, view_name='comment-detail', read_only=True)
+    added_actors = serializers.HyperlinkedRelatedField(many=True, view_name='actor-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups', 'reviews')
+        fields = ('url', 'username', 'email', 'groups', 'reviews', 'comments', 'added_actors')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
@@ -24,34 +27,136 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
+    queryset_categories = Category.objects.all()
+    queryset_actors = Actor.objects.all()
+
+    # id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=200)
+    length = serializers.CharField(required=False, allow_blank=True, max_length=10)
+    related_categories = serializers.HyperlinkedRelatedField(
+        many=True,
+        queryset=queryset_categories,
+        view_name='category-detail'
+    )
+    related_actors = serializers.HyperlinkedRelatedField(
+        many=True,
+        queryset=queryset_actors,
+        view_name='actor-detail'
+    )
 
-    """
-    This is the manual way to define create and update functions
-    def create(self, validated_data):
-
-        #Create and return a new `Movie` instance, given the validated data.
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-
-        #Update and return an existing `Movie` instance, given the validated data.
-        instance.title = validated_data.get('title', instance.title)
-        instance.save()
-        return instance
-    """
+    # TODO: Program average rating from reviews
+    rating = serializers.IntegerField()
 
     class Meta:
         model = Movie
-        fields = ('id', 'title')
+        fields = ('id', 'title', 'length', 'rating', 'related_categories', 'related_actors')
+
+    def display_value(self, instance):
+        return 'Movie: %s' % (instance.title)
 
 
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    text = serializers.CharField(required=False, allow_blank=True, max_length=200)
-    owner = serializers.ReadOnlyField(source='owner.username')
+    queryset_movie = Movie.objects.all()
+
+    # id = serializers.IntegerField(read_only=True)
+    text = serializers.CharField(required=False, allow_blank=True, max_length=3000)
+    rating = serializers.IntegerField(required=True)
+    related_user = serializers.ReadOnlyField(source='related_user.username')
+    related_movie = serializers.HyperlinkedRelatedField(
+        queryset=queryset_movie,
+        view_name='movie-detail'
+    )
 
     class Meta:
         model = Review
-        fields = ('id', 'text', 'owner')
+        fields = ('id', 'text', 'rating', 'related_user', 'related_movie')
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    queryset_movie = Movie.objects.all()
+
+    # id = serializers.IntegerField(read_only=True)
+    text = serializers.CharField(required=False, allow_blank=True, max_length=400)
+
+    # TODO: Make a proper timestamp format (00:00:00)
+    timestamp = serializers.CharField(required=False, allow_blank=True, max_length=30)
+
+    related_user = serializers.ReadOnlyField(source='related_user.username')
+    related_movie = serializers.HyperlinkedRelatedField(
+        queryset=queryset_movie,
+        view_name='movie-detail'
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'timestamp', 'related_user', 'related_movie')
+
+
+class ActorSerializer(serializers.HyperlinkedModelSerializer):
+    # id = serializers.IntegerField(read_only=True)
+    firstname = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    lastname = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    related_user = serializers.ReadOnlyField(source='related_user.username')
+
+    class Meta:
+        model = Actor
+        fields = ('id', 'firstname', 'lastname', 'related_user')
+
+
+class MovieActorSerializer(serializers.HyperlinkedModelSerializer):
+    queryset_movie = Movie.objects.all()
+    queryset_actor = Movie.objects.all()
+
+    # id = serializers.IntegerField(read_only=True)
+    related_actor = serializers.HyperlinkedRelatedField(
+        queryset=queryset_actor,
+        view_name='actor-detail'
+    )
+    related_movie = serializers.HyperlinkedRelatedField(
+        queryset=queryset_movie,
+        view_name='actor-detail'
+    )
+
+    class Meta:
+        model = MovieActor
+        fields = ('id', 'related_actor', 'related_movie')
+
+
+class TrailerSerializer(serializers.HyperlinkedModelSerializer):
+    queryset_movie = Movie.objects.all()
+
+    # id = serializers.IntegerField(read_only=True)
+    related_user = serializers.ReadOnlyField(source='related_user.username')
+    related_movie = serializers.HyperlinkedRelatedField(
+        queryset=queryset_movie,
+        view_name='actor-detail'
+    )
+    video_path = serializers.CharField(required=False, allow_blank=True, max_length=500)
+
+    class Meta:
+        model = Trailer
+        fields = ('id', 'video_path', 'related_user', 'related_movie')
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    # id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
+class MovieCategorySerializer(serializers.HyperlinkedModelSerializer):
+    queryset_movie = Movie.objects.all()
+
+    # id = serializers.IntegerField(read_only=True)
+    related_category = serializers.ReadOnlyField(source='related_category.name')
+    related_movie = serializers.HyperlinkedRelatedField(
+        queryset=queryset_movie,
+        view_name='actor-detail'
+    )
+
+    class Meta:
+        model = MovieCategory
+        fields = ('id', 'related_category', 'related_movie')
--- movieapi/app/views.py
+++ movieapi/app/views.py
@@ -12,8 +12,8 @@ from rest_framework import permissions
 
 from movieapi.app.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User, Group
-from movieapi.app.models import Movie, Review
-from movieapi.app.serializers import UserSerializer, GroupSerializer, MovieSerializer, ReviewSerializer
+from movieapi.app.models import Movie, Review, Comment, Actor, MovieActor, Trailer, Category, MovieCategory
+from movieapi.app.serializers import UserSerializer, GroupSerializer, MovieSerializer, ReviewSerializer, CommentSerializer, ActorSerializer, MovieActorSerializer, TrailerSerializer, CategorySerializer, MovieCategorySerializer
 
 """
 TODO:
@@ -22,31 +22,46 @@ TODO:
 - Remove unused imports
 """
 
+"""
+MAIN VIEW
+"""
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         'users': reverse('user-list', request=request, format=format),
-        'reviews': reverse('review-list', request=request, format=format)
+        'groups': reverse('group-list', request=request, format=format),
+        'reviews': reverse('review-list', request=request, format=format),
+        'comments': reverse('comment-list', request=request, format=format),
+        'actors': reverse('actor-list', request=request, format=format),
+        'categories': reverse('category-list', request=request, format=format),
+        'movies': reverse('movie-list', request=request, format=format)
     })
 
 
 """
-REVIEW
+USER
 """
-class ReviewList(generics.ListCreateAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    #Override perform_create
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+"""
+GROUP
+"""
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 
 """
@@ -63,13 +78,102 @@ class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 """
-USER
+REVIEW
 """
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    #Override perform_create
+    def perform_create(self, serializer):
+        serializer.save(related_user=self.request.user)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+"""
+COMMENT
+"""
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(related_user=self.request.user)
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+"""
+ACTOR
+"""
+class ActorList(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(related_user=self.request.user)
+
+class ActorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+"""
+MOVIEACTOR
+"""
+class MovieActorList(generics.ListCreateAPIView):
+    queryset = MovieActor.objects.all()
+    serializer_class = MovieActorSerializer
+
+
+class MovieActorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MovieActor.objects.all()
+    serializer_class = MovieActorSerializer
+
+
+"""
+TRAILER
+"""
+class TrailerList(generics.ListCreateAPIView):
+    queryset = Trailer.objects.all()
+    serializer_class = TrailerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(related_user=self.request.user)
+
+class TrailerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Trailer.objects.all()
+    serializer_class = TrailerSerializer
+
+
+"""
+CATEGORY
+"""
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+"""
+MOVIECATEGORY
+"""
+class MovieCategoryList(generics.ListCreateAPIView):
+    queryset = MovieCategory.objects.all()
+    serializer_class = MovieCategorySerializer
+
+
+class MovieCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MovieCategory.objects.all()
+    serializer_class = MovieCategorySerializer
