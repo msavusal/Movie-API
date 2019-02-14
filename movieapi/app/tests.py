2447fc6b5cd9c3022638075f56bfe66d1e6ccd6e
msavusal <markus.savusalo@windowslive.com>
2019-02-14 12:39:04 +0200

    Added test cases for models to tests.py

--- movieapi/app/tests.py
+++ movieapi/app/tests.py
@@ -19,9 +19,57 @@ Test case for Review model
 """
 class ReviewTestCase(TestCase):
     def setUp(self):
-        Review.objects.create(title="TestReview", length="00:05:33", rating="5")
+        Review.objects.create(title="TestReview", length="00:05:33", rating="3")
 
     def test_object_creation(self):
         print("Testing object creation for object Review")
         testReview = Review.objects.get(title="TestReview")
         self.assertEqual(testReview.title, 'TestReview')
+        
+"""
+Test case for Comment model
+"""
+class CommentTestCase(TestCase):
+    def setUp(self):
+        Comment.objects.create(title="TestComment", length=21)
+
+    def test_object_creation(self):
+        print("Testing object creation for object Comment")
+        testComment = Comment.objects.get(title="TestComment")
+        self.assertEqual(testComment.title, 'TestComment')
+        
+"""
+Test case for Actor model
+"""
+class ActorTestCase(TestCase):
+    def setUp(self):
+        Actor.objects.create(title="TestActor", name="John Doe", age=27)
+
+    def test_object_creation(self):
+        print("Testing object creation for object Actor")
+        testActor = Actor.objects.get(title="TestActor")
+        self.assertEqual(testActor.title, 'TestActor')
+        
+"""
+Test case for MovieActor model
+"""
+class MovieActorTestCase(TestCase):
+    def setUp(self):
+        MovieActor.objects.create(title="TestMovieActor", name="John Doe", age=27)
+
+    def test_object_creation(self):
+        print("Testing object creation for object MovieActor")
+        testMovie = MovieActor.objects.get(title="TestMovie")
+        self.assertEqual(testMovie.title, 'TestMovie')
+
+"""
+Test case for Trailer model
+"""
+class TrailerTestCase(TestCase):
+    def setUp(self):
+        Trailer.objects.create(title="TestTrailer", length="00:02:11", rating="5")
+
+    def test_object_creation(self):
+        print("Testing object creation for object Trailer")
+        testTrailer = Trailer.objects.get(title="TestTrailer")
+        self.assertEqual(testTrailer.title, 'TestTrailer')        
