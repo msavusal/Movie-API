3fb27c8c8d4f2491ca9d6d66b201105988b8f3f9
Jaakko Seppänen <seppanenj91@gmail.com>
2019-04-25 13:39:03 +0300

    Changed app

--- Exercises/app.py
+++ Exercises/app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, request, json
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
@@ -11,9 +21,9 @@ class StorageItem(db.Model):
     qty = db.Column(db.Integer, nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
     location = db.Column(db.String(64), nullable=False)
-    
+
     product = db.relationship("Product", back_populates="products")
-    
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     handle = db.Column(db.String(64), unique=True, nullable=False)
@@ -21,13 +31,21 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
 
     products = db.relationship("StorageItem", back_populates="product")
-    
+
+deployments = db.Table("deployments",
+    db.Column("storage_item_id", db.Integer, db.ForeignKey("storage_item.id"), primary_key=True),
+    db.Column("product_id", db.Integer, db.ForeignKey("product.id"), primary_key=True)
+)
+
+db.create_all()
+
 @app.route("/products/add", methods=["POST"])
-def add_product(product_name):
+def add_product():
     if request.method == "POST":
     # This branch happens when user submits the form
         try:
-            product = Product.query.filter_by(name=product_name).first()
+            product_name = str(request.json["product"])
+            product = Product.query.filter_by(handle=product_name).first()
             if product:
                 handle = str(request.json["handle"])
                 weight = float(request.json["weight"])
@@ -53,49 +71,14 @@ def add_product(product_name):
         except IntegrityError:
             return "POST method required", 405
     return 201
-    
-    
+
+
 @app.route("/storage/<product>/add", methods=["POST"])
-def add_to_storage(storage_name):
+def add_to_storage(product):
     if request.method == "POST":
     # This branch happens when user submits the form
         try:
-            storage = StorageItem.query.filter_by(name=storage_name).first()
-            if storage:
-                id=id
-                qty=int(request.json["qty"])
-                product_id=product_id
-                location=str(request.json["location"])
-                stor = Storage(
-                    id=id,
-                    qty=qty,
-                    product_id=product_id,
-                    location=location
-                )
-                db.session.add(stor)
-                db.session.commit()
-            else:
-                abort(404)    
-        except NameError:
-            return "Handle already exists", 409
-        except KeyError:
-            return "Incomplete request - missing fields", 400
-        except TypeError:
-            return "Product not found", 404
-        except ValueError:
-            return "Qty must be an integer", 400
-        except IntegrityError:
-            return "POST method required", 405
-        except IntegrityError:
-            return "Request content type must be JSON", 415
-    return 201
-        
-@app.route("/storage/", methods=["GET"])
-def get_inventory(req):
-    if request.method == "GET":
-    # This branch happens when user submits the form
-        try:
-            storage = StorageItem.query.filter_by(name=storage_name).first()
+            storage = StorageItem.query.filter_by(name=product).first()
             if storage:
                 id=id
                 qty=int(request.json["qty"])
@@ -111,23 +94,24 @@ def get_inventory(req):
                 db.session.commit()
             else:
                 abort(404)
+        except NameError:
+            return "Handle already exists", 409
+        except KeyError:
+            return "Incomplete request - missing fields", 400
+        except TypeError:
+            return "Product not found", 404
+        except ValueError:
+            return "Qty must be an integer", 400
         except IntegrityError:
-            return "GET method required", 405
-    return 200
-        
+            return "POST method required", 405
+        except IntegrityError:
+            return "Request content type must be JSON", 415
+    return 201
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route("/storage/", methods=["GET"])
+def get_inventory():
+    response_data = []
+    storage = StorageItem.query.all()
+    for product in storage:
+        response_data.append([product.handle, product.weight])
+    return json.dumps(response_data)
