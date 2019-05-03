98bb6d4b0543f849c1d8ac69d3dc2683f9cd1fdb
Jaakko Seppänen <seppanenj91@gmail.com>
2019-05-03 16:12:45 +0300

    Fixed functions

--- Exercises/app.py
+++ Exercises/app.py
@@ -40,77 +40,86 @@ deployments = db.Table("deployments",
 db.create_all()
 
 @app.route("/products/add/", methods=["POST"])
-def add_product(product_name):
-    if request.method == "POST":
-    # This branch happens when user submits the form
-        try:
-            product = Product.query.filter_by(handle=product_name).first()
-            if product:
-                handle = str(request.json["handle"])
-                weight = float(request.json["weight"])
-                price = float(request.json["price"])
-                prod = Product(
-                    handle=handle,
-                    weight=weight,
-                    price=price
-                )
-                db.session.add(prod)
-                db.session.commit()
-            else:
-                abort(404)
-        except NameError:
-            return "Handle already exists", 409
-        except KeyError:
-            return "Incomplete request - missing fields", 400
-        except TypeError:
-            return "Request content type must be JSON", 415
-        except ValueError:
-            return "Weight and price must be numbers", 400
-        except IntegrityError:
-            return "POST method required", 405
-    return 201
+def add_product():
+    try:
+        handle_name = request.json["handle"]
+        product = Product.query.filter_by(handle=handle_name).first()
+        if product:
+            raise NameError
+
+        handle = str(request.json["handle"])
+        weight = float(request.json["weight"])
+        price = float(request.json["price"])
+        prod = Product(
+            handle=handle,
+            weight=weight,
+            price=price
+        )
+        db.session.add(prod)
+        db.session.commit()
+
+    except NameError:
+        return "Handle already exists", 409
+    except KeyError:
+        return "Incomplete request - missing fields", 400
+    except TypeError:
+        return "Request content type must be JSON", 415
+    except ValueError:
+        return "Weight and price must be numbers", 400
+    except IntegrityError:
+        return "POST method required", 405
+
+    return "Created.", 201
 
 
 @app.route("/storage/<product>/add/", methods=["POST"])
 def add_to_storage(product):
-    if request.method == "POST":
-    # This branch happens when user submits the form
-        try:
-            storage = StorageItem.query.filter_by(name=product).first()
-            if storage:
-                id=id
-                qty=int(request.json["qty"])
-                product_id=product_id
-                location=str(request.json["location"])
-                stor = Storage(
-                    id=id,
-                    qty=qty,
-                    product_id=product_id,
-                    location=location
-                )
-                db.session.add(stor)
-                db.session.commit()
-            else:
-                abort(404)
-        except NameError:
-            return "Handle already exists", 409
-        except KeyError:
-            return "Incomplete request - missing fields", 400
-        except TypeError:
-            return "Product not found", 404
-        except ValueError:
-            return "Qty must be an integer", 400
-        except IntegrityError:
-            return "POST method required", 405
-        except IntegrityError:
-            return "Request content type must be JSON", 415
-    return 201
+    try:
+        product = Product.query.filter_by(handle=product).first()
+        if product:
+            qty=int(request.json["qty"])
+            location=str(request.json["location"])
+            stor = StorageItem(
+                qty=qty,
+                product_id=product.id,
+                location=location
+            )
+            db.session.add(stor)
+            db.session.commit()
+        else:
+            raise TypeError
+            
+    except KeyError:
+        return "Incomplete request - missing fields", 400
+    except TypeError:
+        return "Product not found", 404
+    except ValueError:
+        return "Qty must be an integer", 400
+    except IntegrityError:
+        return "POST method required", 405
+    except IntegrityError:
+        return "Request content type must be JSON", 415
+
+    return "Created.", 201
+
 
 @app.route("/storage/", methods=["GET"])
 def get_inventory():
+
     if request.method == "GET":
         response_data = []
-        storage = StorageItem.query.all()
-        for product in storage:
-            response_data.append([product.handle, product.weight, product.price, product.inventory])
+        products = Product.query.all()
+
+        for product in products:
+            inventory = []
+            for inv in product.products:
+                inventory.append([inv.location, inv.qty])
+
+            response_data.append({
+                "handle": product.handle,
+                "weight": product.weight,
+                "price": product.price,
+                "inventory": inventory,
+            })
+
     return json.dumps(response_data)
